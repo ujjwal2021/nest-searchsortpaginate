2cf78e41dcb7e86bf3eb0b39653dbc1fcdbadf55
ujjwal <bhattaraiujjwal26@gmail.com>
2022-02-28 23:46:52 +0530

    product controller and services modified

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -14,28 +14,6 @@ export class ProductController {
 
     @Get('backend')
     async backend(@Req() req: Request){
-        const builder = await this.productService.searchProducts('products');
-        if(req.query.keyword){
-            builder.where("products.title LIKE :keyword OR products.description LIKE :keyword", {keyword: `%${req.query.keyword}%`});
-        }
-        const sort : any = req.query.sort;
-        if(sort){
-            builder.orderBy('products.price', sort.toUpperCase());
-        }
-
-        const page: number =parseInt(req.query.page as any) || 1;
-        const perpage = 9;
-        const total = await builder.getCount();
-
-        builder.offset((page-1)*perpage).limit(perpage);
-
-        return{
-            total,
-            page,
-            last_page: Math.ceil(total/perpage),
-            data: await builder.getMany(),
-        }
-
-
+        return this.productService.searchProducts(req);
     }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Req } from '@nestjs/common';
+import {Request} from 'express';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './product.entity';
@@ -14,7 +15,29 @@ export class ProductService {
         return this.productRepository.find();
     }
 
-    async searchProducts(alias: string){
-        return this.productRepository.createQueryBuilder(alias);
+    async searchProducts(req: Request){
+        // return this.productRepository.createQueryBuilder(alias);
+        const builder = await this.productRepository.createQueryBuilder('products');
+        if(req.query.keyword){
+            builder.where("products.title LIKE :keyword OR products.description LIKE :keyword", {keyword: `%${req.query.keyword}%`});
+        }
+        const sort : any = req.query.sort;
+        if(sort){
+            builder.orderBy('products.price', sort.toUpperCase());
+        }
+
+        const page: number =parseInt(req.query.page as any) || 1;
+        const perpage = 9;
+        const total = await builder.getCount();
+
+        builder.offset((page-1)*perpage).limit(perpage);
+
+        return{
+            total,
+            page,
+            last_page: Math.ceil(total/perpage),
+            data: await builder.getMany(),
+        }
+
     }
 }
